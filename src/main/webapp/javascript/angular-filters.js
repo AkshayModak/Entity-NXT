bdbdcf56a5a0abdf05dd1b00b757f005ab9c0555
Akshay Modak <akshay.modak@hotwaxsystems.com>
2018-01-29 23:10:02 +0530

    Removed unwanted code and files.

--- src/main/webapp/javascript/angular-filters.js
+++
@@ -1,8 +0,0 @@
-// filter to reverse angularjs list
-// Reference -- https://stackoverflow.com/questions/15266671/angular-ng-repeat-in-reverse
-myApp.filter('reverse', function() {
-  return function(items) {
-	if (!items || !items.length) { return; }
-    return items.slice().reverse();
-  };
-});
